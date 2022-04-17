63c3b885b312494d41357a12cd68e449d89a96fa
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-16 23:55:01 -0700

    adds index action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <script>
+import axios from 'axios'
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      dishes: []
     };
   },
-  created: function () { },
-  methods: {},
+  created: function () {
+    this.indexDishes()
+  },
+  methods: {
+    indexDishes: function () {
+      console.log('all products')
+      axios.get("/dishes").then(response => {
+        console.log(response.data)
+        this.dishes = response.data
+      })
+    }
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h3>All dishes</h3>
+    <div v-for="dish in dishes" v-bind:key="dish.id">
+      <p>{{ dish.name }}</p>
+    </div>
   </div>
 </template>
 
